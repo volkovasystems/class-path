570d5c16fda7621a18c24a71d99388580f61f410
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-07-10 07:13:11 +0800

    Updated test for class Path module to use mocha and shouldjs.

--- class-path-test.js
+++ class-path-test.js
@@ -1,83 +1,87 @@
-var assert = require( "assert" );
-
 var Path = require( "./class-path.js" );
 
 describe( "Path class",
-    function PathClass( ){
-        //var testedMethodList = [ ];
+	function PathClass( ){
 
 		it( "should have verifyIfExisting method.",
 			function shouldHaveVerifyIfExistingMethod( ){
-                Path.prototype.should.have.property( "verifyIfExisting" );
-                Path.prototype.property( "verifyIfExisting" ).should.be.a.Function;
-				/*assert.ifTrue( "verifyIfExisting" in Path.prototype ).toBe( true );
-				expect( typeof Path.prototype.verifyIfExisting ).toBe( "function" );
-                testedMethodList.push( "verifyIfExisting" );*/
+
+				Path.prototype.should.have.property( "verifyIfExisting" );
+				
+				Path.prototype.verifyIfExisting.should.be.a.Function;
 			} );
 
-		/*it( "should have verifyIfEmpty method.",
+		it( "should have verifyIfEmpty method.",
 			function shouldHaveVerifyIfEmptyMethod( ){
-				expect( "verifyIfEmpty" in Path.prototype ).toBe( true );
-				expect( typeof Path.prototype.verifyIfEmpty ).toBe( "function" );
-                testedMethodList.push( "verifyIfEmpty" );
+
+				Path.prototype.should.have.property( "verifyIfEmpty" );
+				
+				Path.prototype.verifyIfEmpty.should.be.a.Function;
 			} );
 
 		it( "should have verifyIfNotEmpty method.",
-			function shouldHaveVerifyIfNotEmptyMethod( ){
-				expect( "verifyIfNotEmpty" in Path.prototype ).toBe( true );
-				expect( typeof Path.prototype.verifyIfNotEmpty ).toBe( "function" );
-                testedMethodList.push( "verifyIfNotEmpty" );
+			function shouldHaveVerifyIfEmptyMethod( ){
+				
+				Path.prototype.should.have.property( "verifyIfNotEmpty" );
+				
+				Path.prototype.verifyIfNotEmpty.should.be.a.Function;
 			} );
 
 		it( "should have checkIfDirectory method.",
 			function shouldHaveCheckIfDirectoryMethod( ){
-				expect( "checkIfDirectory" in Path.prototype ).toBe( true );
-				expect( typeof Path.prototype.checkIfDirectory ).toBe( "function" );
-                testedMethodList.push( "checkIfDirectory" );
+				
+				Path.prototype.should.have.property( "checkIfDirectory" );
+				
+				Path.prototype.checkIfDirectory.should.be.a.Function;
 			} );
 
 		it( "should have checkIfFile method.",
 			function shouldHaveCheckIfFileMethod( ){
-				expect( "checkIfFile" in Path.prototype ).toBe( true );
-				expect( typeof Path.prototype.checkIfFile ).toBe( "function" );
-                testedMethodList.push( "checkIfFile" );
+				
+				Path.prototype.should.have.property( "checkIfFile" );
+
+				Path.prototype.checkIfFile.should.be.a.Function;
 			} );
 
 		it( "should have appendToPath method.",
 			function shouldHaveAppendToPathMethod( ){
-				expect( "appendToPath" in Path.prototype ).toBe( true );
-				expect( typeof Path.prototype.appendToPath ).toBe( "function" );
-                testedMethodList.push( "appendToPath" );
+
+				Path.prototype.should.have.property( "appendToPath" );
+
+				Path.prototype.appendToPath.should.be.a.Function;
 			} );
 
 		it( "should have appendToRawPath method.",
 			function shouldHaveAppendToRawPathMethod( ){
-				expect( "appendToRawPath" in Path.prototype ).toBe( true );
-				expect( typeof Path.prototype.appendToRawPath ).toBe( "function" );
-                testedMethodList.push( "appendToRawPath" );
+				
+				Path.prototype.should.have.property( "appendToRawPath" );
+
+				Path.prototype.appendToRawPath.should.be.a.Function;
 			} );
 
 		it( "should have joinToPath method.",
 			function shouldHaveJoinToPathMethod( ){
-				expect( "joinToPath" in Path.prototype ).toBe( true );
-				expect( typeof Path.prototype.joinToPath ).toBe( "function" );
-                testedMethodList.push( "joinToPath" );
+				
+				Path.prototype.should.have.property( "joinToPath" );
+
+				Path.prototype.joinToPath.should.be.a.Function;
 			} );
 
 		it( "should have joinToRawPath method.",
 			function shouldHaveJoinToRawPathMethod( ){
-				expect( "joinToRawPath" in Path.prototype ).toBe( true );
-				expect( typeof Path.prototype.joinToRawPath ).toBe( "function" );
-                testedMethodList.push( "joinToRawPath" );
-			} );*/
+				
+				Path.prototype.should.have.property( "joinToRawPath" );
 
-        /*var defaultMethodList = [ "constructor", "toString" ];
-        var PathClassPrototypePropertyList = Object.getOwnPropertyNames( Path.prototype );
-        var allMethodList = defaultMethodList.concat( testedMethodList );
+				Path.prototype.joinToRawPath.should.be.a.Function;
+			} );
 
-        it( "should have " + testedMethodList.length + " methods in the current class.",
-            function shouldHaveNMethodInTheCurrentClass( ){
-                expect( allMethodList.length ).toEqual( PathClassPrototypePropertyList.length );
-            } );
+		/*var defaultMethodList = [ "constructor", "toString" ];
+		var PathClassPrototypePropertyList = Object.getOwnPropertyNames( Path.prototype );
+		var allMethodList = defaultMethodList.concat( testedMethodList );
+
+		it( "should have " + testedMethodList.length + " methods in the current class.",
+			function shouldHaveNMethodInTheCurrentClass( ){
+				expect( allMethodList.length ).toEqual( PathClassPrototypePropertyList.length );
+			} );
 */
-    } );
+	} );
