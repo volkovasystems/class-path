82ebf6321ab418c5ed6924ccbf7f27bd5651a342
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-07-12 21:31:39 +0800

    Added module license. Added decomposePathToList procedure. Added verifyIfNotExisting procedure.

--- class-path.js
+++ class-path.js
@@ -1,4 +1,28 @@
 /*:
+	@module-license:
+		The MIT License (MIT)
+
+		Copyright (c) 2014 Richeve Siodina Bebedor
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
+	@end-module-license
+
 	@module-configuration:
 		{
 			"packageName": "class-path",
@@ -70,6 +94,10 @@ Path.prototype.verifyIfExisting = function verifyIfExisting( ){
 	return fs.existsSync( this.toString( ) );
 };
 
+Path.prototype.verifyIfNotExisting = function verifyIfNotExisting( ){
+	return !this.verifyIfExisting( );
+};
+
 Path.prototype.verifyIfEmpty = function verifyIfEmpty( ){
 	if( this.checkIfDirectory( ) ){
 		return fs.readdirSync( this.toString( ) ).length > 0;
@@ -95,7 +123,6 @@ Path.prototype.checkIfDirectory = function checkIfDirectory( ){
     }else{
         return false;
     }
-
 };
 
 Path.prototype.checkIfFile = function checkIfFile( ){
@@ -163,6 +190,10 @@ Path.prototype.joinToRawPath = function joinToRawPath( location ){
 	newPath.appendToRawPath( location );
 
 	return newPath;
+};
+
+Path.prototype.decomposePathToList = function decomposePathToList( ){
+	return this.toString( ).split( path.sep );
 };
 
 Path.prototype.toString = function toString( ){
